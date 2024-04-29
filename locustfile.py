2e5437fb1fc7d9af49b33569d09592ad9558e916
Passawee Wanwilai <94208246+PassaweeTT@users.noreply.github.com>
2024-04-29 09:48:33 +0700

    fix text

--- locustfile.py
+++ locustfile.py
@@ -2,6 +2,7 @@ from locust import HttpUser, task, between
 import csv
 import random
 
+
 class ApiUser(HttpUser):
     wait_time = between(1, 5)
 
@@ -13,10 +14,21 @@ class ApiUser(HttpUser):
     @task
     def get_top_headlines(self):
         country_code = random.choice(self.countries).lower()
-        url = f"https://newsapi.org/v2/top-headlines?country={country_code}&apiKey=8a95d6fc34c845a5a6467fea09f93bcd"
+        url = f"""https://newsapi.org/v2/top-headlines?country={
+            country_code}&apiKey=8a95d6fc34c845a5a6467fea09f93bcd"""
         response = self.client.get(url)
         if response.status_code == 200:
             articles = response.json().get("articles", [])
-            print(f"Top headlines from {country_code}")
+            print(f'Top headlines from "{country_code}"')
             for article in articles[:5]:
-                print(f"------------------------------------------------------------------\nTitle: {article.get('title')} \nSource: {article.get('source', {}).get('name')} \n------------------------------------------------------------------\n")
+                print(
+                    "------------------------------------------------------------------")
+                print(f"""Title: {article.get('title')} \nSource: {
+                      article.get('source', {}).get('name')} """)
+                print(
+                    "------------------------------------------------------------------\n")
+        else:
+            print("------------------------------------------------------------------")
+            print(f'Failed to get top headlines from "{country_code}"')
+            print(response.json().get("message", "Unknown error"))
+            print("------------------------------------------------------------------\n")
